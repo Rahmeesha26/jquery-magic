8503cce239ea6b3ccc87b548c4e0202faa0e7a8d
rahmeesha26 <rahmeeshar91@gmail.com>
2023-06-19 16:29:20 -0400

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -7,33 +7,44 @@
 // };
 
 $(() => {
-          // made a div with id of container
+// made a div with id of container
           const $container = $("#container")
           console.log($container)
 
-          // creating a h1 element
+// creating a h1 element and appending it to the div
           const $h1 = $("<h1>")
           $h1.text("Hogwarts, the school for wizards")
           console.log($("h1"))
           $("#container").append($h1)
 
-          // creating a h2, h3, and h4 element with some text
+// creating a h2 and appending it to the div
           const $h2 = $("<h2>")
           $h2.text("Rahmeesha Reynolds")
           $("#container").append($h2)
           console.log($("h2"))
 
+// creating a h3 and appending it to the div
           const $h3 = $("<h3>")
           $h3.text("Gryffindor")
           $("#container").append($h3)
           console.log($("h3"))
 
-
+// creating a h4 and appending it to the div
           const $h4 = $(`<h4 class="cat">Eightball</h4>`)
           $("#container").append($h4)
           $("#container").append(`<h4>Holly Wand with Unicorn Hair Core</h4>`)
           console.log($("h4"))
 
-
+// creating a few li's for the ul 
+          $("ul").append(`<li class="secret">invisibility cloak</li>`)
+          $("ul").append(`<li class="secret">magic map</li>`)
+          $("ul").append(`<li class="secret">time turner</li>`)
+          $("ul").append(`<li class="cat">leash</li>`)
+          $("ul").append(`<li>Bertie Bott's Every Flavor jelly beans</li>`)
+          $("ul").append(`<li>butter beer</li>`)
+          $("#container").append("<ul>")
+          
+          
+          
 
 });
