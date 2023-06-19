16cfe4b9779d5632f21d0ea5acc702e770db84fb
rahmeesha26 <rahmeeshar91@gmail.com>
2023-06-18 20:03:09 -0400

    Commit 0 - All my files are linked and loaded

---
+++ app.js
@@ -0,0 +1,7 @@
+const jquery = require("jquery");
+
+if (typeof jquery == 'undefined'){
+          // console.log('oops! I still have to link my jQuery properly!');
+} else {
+          // console.log('I did it! I linked jQuery and this js file!')
+};
